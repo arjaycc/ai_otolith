6cdfad27754a4d4ca48e2ec71aec5574bbf314b0
accayetano <accayetano@PCSF169061-BHV.ad.vti.bund.de>
2024-02-20 16:06:03 +0100

    added missing files + source

--- imgproc/imgproc_aux.py
+++ imgproc/imgproc_aux.py
@@ -166,6 +166,7 @@ def get_major_axes(ncontour, cx, cy):
 
 def peakdet(v, delta, x = None):
     """
+    https://gist.github.com/endolith/250860
     Converted from MATLAB script at http://billauer.co.il/peakdet.html
     % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
     % This function is released to the public domain; Any use is allowed.
